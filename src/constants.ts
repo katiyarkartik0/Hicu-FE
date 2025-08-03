798bc50b447db32bcd8c30dd331bc314be9726d8
katiyarkartik0@gmail.com <kartik.katiyar@newtonschool.co>
2025-08-03 22:33:42 +0530

    changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 let API_BASE_URL = "";
 
 if (process.env.NODE_ENV === "production") {
-  API_BASE_URL = "http://localhost:5000";
+  API_BASE_URL = "https://hicu-backend.onrender.com";
 } else {
   API_BASE_URL = "http://localhost:5000";
 }
